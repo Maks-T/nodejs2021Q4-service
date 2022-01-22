9203ef285f52c6661f45d985b7f81222fc24ba86
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-01-22 16:15:19 +0300

    feat: auth service

---
+++ src/resources/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { userRepo } from '../users/user.repository';
+import { IAuth } from './auth.model';
+import * as bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { config } from '../../common/config';
+
+const getToken = async (
+  authData: IAuth
+): Promise<{ token: string } | undefined> => {
+  const { login, password } = authData;
+  const user = await userRepo().findOne({ login });
+  if (
+    user &&
+    (await bcrypt.compare(password, user.password)) &&
+    config.JWT_SECRET_KEY
+  ) {
+    const payload = { userId: user.id, login };
+    const token = jwt.sign(payload, config.JWT_SECRET_KEY, {
+      expiresIn: 3600,
+    });
+    return { token };
+  }
+
+  return undefined;
+};
+
+export default { getToken };
